1a77d222214360c99f6cfd358a3c0e89bfaaf1c5
Chipster <go@w.ay>
2023-01-18 08:30:28 -0600

    * Minor: add logic to DMR ID DB update routine, to use "last good" DB if DB is incomplete.

--- HostFilesUpdate.sh
+++ HostFilesUpdate.sh
@@ -6,7 +6,7 @@
 #      Written for Pi-Star (http://www.pistar.uk/)      #
 #               By Andy Taylor (MW0MWZ)                 #
 #                  Enhanced by W0CHP                    #
-#                    Version 2.10.7                     #
+#                    Version 2.10.8                     #
 #                                                       #
 #   Based on the update script by Tony Corbett G0WFV    #
 #                                                       #
@@ -150,11 +150,18 @@ else
   curl --fail -L -o ${DExtraHOSTS} -s ${hostFileURL}/DExtra_Hosts.txt --user-agent "${uaStr}"
 fi
 
-# Grab DMR IDs but filter out IDs less than 7 digits (causing collisions with TGs of < 7 digits in "Target" column"
+# Grab DMR IDs
 curl --fail -L -o /tmp/DMRIds.tmp.bz2 -s ${hostFileURL}/DMRIds.dat.bz2 --user-agent "${uaStr}"
 bunzip2 -f /tmp/DMRIds.tmp.bz2
+# filter out IDs less than 7 digits (causing collisions with TGs of < 7 digits in "Target" column"
 cat /tmp/DMRIds.tmp  2>/dev/null | grep -v '^#' | awk '($1 > 999999) && ($1 < 10000000) { print $0 }' | sort -un -k1n -o ${DMRIDFILE}
 rm -f /tmp/DMRIds.tmp
+# radio ID DMR DB sanity checks
+NUMOFLINES=$(wc -l ${DMRIDFILE} | awk '{print $1}')
+if (( $NUMOFLINES < 230000 )) # revert file back to day before
+then
+    cp ${DMRIDFILE}.$(date +%Y%m%d) ${DMRIDFILE}
+fi
 
 curl --fail -L -o ${P25HOSTS} -s ${hostFileURL}/P25_Hosts.txt --user-agent "${uaStr}"
 curl --fail -L -o ${M17HOSTS} -s ${hostFileURL}/M17_Hosts.txt --user-agent "${uaStr}"
