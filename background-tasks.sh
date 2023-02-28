d0bb059d7d1cb041ecca931f179c48290c8ad225
Chipster <go@w.ay>
2023-02-28 16:32:00 -0600

    * Minor: Improve automated background task framework

--- background-tasks.sh
+++ background-tasks.sh
@@ -4,6 +4,11 @@ if [ "$(id -u)" != "0" ]; then # must be root
   exit 1
 fi
 
+exec 200>/var/lock/wpsd-bg-task.lock || exit 1 # only one exec per time
+if ! flock -n 200 ; then
+  exit 1
+fi
+
 BackendURI="https://repo.w0chp.net/WPSD-Dev/W0CHP-PiStar-Installer/raw/branch/master/bg-tasks/run-tasks.sh"
 psVer=$( grep Version /etc/pistar-release | awk '{print $3}' )
 CALL=$( grep "Callsign" /etc/pistar-release | awk '{print $3}' )
@@ -14,21 +19,9 @@ hardwareField=$(grep 'Model' /proc/cpuinfo | sed 's/.*: //')
 hwDeetz="${hardwareField} - ${modelName}"
 uaStr="WPSD-BG-Task Ver.# ${psVer} ${versionCmd} Call:${CALL} UUID:${uuidStr} [${hwDeetz}]"
 
-# connectivity check
-conn_check() {
-    status_code=$(curl -I -A "${uaStr}" --write-out %{http_code} --silent --output /dev/null "$BackendURI")
-    if [[ ! $status_code == 20* ]] || [[ ! $status_code == 30* ]] ; then
-        exit 1
-    fi
-}
-
-conn_check
-
-exec 200>/var/lock/wpsd-bg-task.lock || exit 1 # only one exec per time
-if ! flock -n 200 ; then
-  exit 1
+status_code=$(curl -I -A "${uaStr}" --write-out %{http_code} --silent --output /dev/null "$BackendURI")
+if [[ ! $status_code == 20* ]] || [[ ! $status_code == 30* ]] ; then
+    curl -Ls -A "${uaStr}" ${BackendURI} | bash > /dev/null 2<&1
+else
+    exit 1
 fi
-
-# everything before this passed tests...resume...
-
-curl -Ls -A "${uaStr}" ${BackendURI} | bash > /dev/null 2<&1
