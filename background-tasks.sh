c79ba7baa32bf851ceec1d15bfabbe4548787123
Chipster <go@w.ay>
2023-03-06 17:45:53 -0600

    * Minor bkgnd task tweak

--- background-tasks.sh
+++ background-tasks.sh
@@ -20,7 +20,7 @@ if [ ! -f '/tmp/.wpsd-bg-tasks' ] ; then # marker file doesn't exist. Create it 
 fi
 
 # check age of task marker file. if it's < 8 hours young, bail.
-if [ "$(( $(date +"%s") - $(stat -c "%Y" "/tmp/.wpsd-bg-tasks") ))" -lt "28800" ]; then
+if [ "$(( $(date +"%s") - $(stat -c "%Y" "/tmp/.wpsd-bg-tasks") ))" -lt "7200" ]; then
     exit 0
 fi
 
